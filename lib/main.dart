5f721cdb03939fd51ec9e78dba2e51ff0d0717a7
Hosung <crm060445@gmail.com>
2024-06-28 17:04:56 +0900

    1.approach 47line 수정함 2.다시 돌려둠 3.호성거 기혁거 다 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:weight_control/screens/approach_selection_screen.dart';
-import 'package:weight_control/screens/pace_selection_screen.dart';
+import 'package:weight_control/screens/height_input_screen.dart';
+import 'package:weight_control/screens/user_weight_screen.dart';
+import 'package:weight_control/screens/email_input_screen.dart';
 import 'package:weight_control/screens/weight_input_screen.dart';
-
+import 'package:weight_control/screens/pace_selection_screen.dart';
+import 'package:weight_control/screens/approach_selection_screen.dart';
 
 void main() {
-runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
+  const MyApp({super.key});
 
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: '체중 관리 앱',
-theme: ThemeData(
-primarySwatch: Colors.green,
-),
-initialRoute: '/',
-routes: {
-'/': (context) => const WeightInputScreen(),
-'/pace-selection': (context) => const PaceSelectionScreen(),
-'/approach-selection': (context) => const ApproachSelectionScreen(),
-},
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '체중 관리 앱',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HeightInputScreen(),
+        '/user-weight': (context) => const UserWeightScreen(),
+        '/email-input': (context) => const EmailInputScreen(),
+        '/weight-input': (context) => const WeightInputScreen(),
+        '/pace-selection': (context) => const PaceSelectionScreen(),
+        '/approach-selection': (context) => const ApproachSelectionScreen(),
+      },
+    );
+  }
 }
